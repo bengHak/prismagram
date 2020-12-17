67517c0c5a4899cd478b18f7de55d0eaf81480e4
bengHak <qkwl4678@naver.com>
2020-12-17 21:40:53 +0900

    sending secret code on email with nodemailer and mailgun

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+// console.log(__dirname);
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({schema});
+const server = new GraphQLServer({ schema });
 
 server.express.use(logger("dev"));
 
-server.start({port: PORT}, () =>
-    console.log(`✅ Server running on port http://localhost:${PORT}`)
+server.start({ port: PORT }, () =>
+  console.log(`✅ Server running on port http://localhost:${PORT}`)
 );
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,41 @@
-import { adjectives, nouns } from "./words"
+import dotenv from "dotenv";
+import path from "path";
+// console.log(__dirname);
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import mgTransport from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
-    const randomNumber = Math.floor(Math.random() * adjectives.length);
-    return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
-}
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = (email) => {
+  const options = {
+    auth: {
+      api_key: process.env.MAILGUN_API,
+      domain: process.env.MAILGUN_DOMAIN,
+    },
+  };
+  const client = nodemailer.createTransport(mgTransport(options));
+  return client
+    .sendMail(email)
+    .then(() => {
+      console.log("Message sent!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "bh@prismagram.com",
+    to: address,
+    subject: "Login Secret for Prismagram 🔐",
+    html: `<h1>Hello! Your login secret is \"${secret}\".</h1> <br/>Copy paste on the app/website to log in.`,
+  };
+  return sendMail(email);
+};
